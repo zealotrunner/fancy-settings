8c20f07f4760bd2741bbf462dda413497002ac5e
Frank Kohlhepp <frnkkpp@googlemail.com>
2011-04-14 17:17:56 +0200

    Testing the tab feature

--- source/settings.js
+++ source/settings.js
@@ -101,4 +101,143 @@ window.addEvent("domready", function () {
         "step": "1"
     });
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    settings.create("Details", "Malfunction", "description", {
+        "text": "Dies sind doch gute Aussichten, oder? Jacken werden für immer schmutzabweisen sein, für immer und ewig in allen Lebenslagen und vor allem auch auf dem Mond. Warum ich hierfür kein Lorem Ipsum verwende, weiß ich auch nicht."
+    });
+    settings.create("Details", "Malfunction", "button", {
+        "label": "Bestellen Sie hier:",
+        "text": "Jetzt kaufen"
+    });
+    settings.create("Details", "Malfunction", "text", {
+        "label": "Oder geben Sie hier ihre Daten ein:",
+        "text": "Name"
+    });
+    settings.create("Details", "Malfunction", "checkbox", {
+        "label": "Bestellung aktivieren"
+    });
+    settings.create("Details", "Malfunction", "slider", {
+        "label": "Größe Ihrer Jacke:",
+        "max": "160",
+        "min": "70",
+        "step": "1"
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    settings.create("Details", "Behaviour", "description", {
+        "text": "Dies sind doch gute Aussichten, oder? Jacken werden für immer schmutzabweisen sein, für immer und ewig in allen Lebenslagen und vor allem auch auf dem Mond. Warum ich hierfür kein Lorem Ipsum verwende, weiß ich auch nicht."
+    });
+    settings.create("Details", "Behaviour", "button", {
+        "label": "Bestellen Sie hier:",
+        "text": "Jetzt kaufen"
+    });
+    settings.create("Details", "Behaviour", "text", {
+        "label": "Oder geben Sie hier ihre Daten ein:",
+        "text": "Name"
+    });
+    settings.create("Details", "Behaviour", "checkbox", {
+        "label": "Bestellung aktivieren"
+    });
+    settings.create("Details", "Behaviour", "slider", {
+        "label": "Größe Ihrer Jacke:",
+        "max": "160",
+        "min": "70",
+        "step": "1"
+    });
+    settings.create("Details", "Behaviour", "popupButton", {
+        "label": "Wählen Sie jetzt noch die Farbe:",
+        "options": [
+            {
+                "value": "blue",
+                "text": "Blau"
+            },
+            {
+                "value": "green",
+                "text": "Grün"
+            },
+            {
+                "value": "red",
+                "text": "Rot"
+            }
+        ]
+    });
+    settings.create("Details", "Behaviour", "listBox", {
+        "label": "Oder wählen Sie sie hier:",
+        "options": [
+            {
+                "value": "blue",
+                "text": "Blau"
+            },
+            {
+                "value": "green",
+                "text": "Grün"
+            },
+            {
+                "value": "red",
+                "text": "Rot"
+            }
+        ]
+    });
+    settings.create("Details", "Behaviour", "radioButtons", {
+        "label": "Oder sogar hier:",
+        "options": [
+            {
+                "value": "blue",
+                "text": "Blau"
+            },
+            {
+                "value": "green",
+                "text": "Grün"
+            },
+            {
+                "value": "red",
+                "text": "Rot"
+            }
+        ]
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 });
