dfc3da351e61bb6f833b7d4ad0a13f473761adef
Frank Kohlhepp <frnkkpp@googlemail.com>
2011-05-19 22:54:55 +0200

    Version 1.0 (beta 1) done

--- source/settings.js
+++ source/settings.js
@@ -1,38 +1,42 @@
 window.addEvent("domready", function () {
     // Option 1: Use the manifest:
+    /*
     new FancySettings.initWithManifest("manifest.json", function (settings) {
         settings.manifestOutput.button1.addEvent("action", function () {
             alert("hello, here i am");
         });
     });
-    
-    /*
-    var f = new FancySettings("TEST", "icon.png");
-    f.create({
-        "name": "test",
-        "type": "text",
-        "tab": "Tab 1",
-        "group": "Group 1",
-        "text": "Drück mich sanft",
-        "label": "Hier:"
-    });
-    f.create({
-        "type": "button",
-        "tab": "Tab 2",
-        "group": "Group 2",
-        "text": "mich auch"
-    });
+    */
     
     // Option 2: Do everything manually:
-    // ---------------------------------
-    // var settings = new FancySettings("My Extension");
-    // 
-    // var checkbox1 = settings.create("tabName", "groupName", "checkbox", {
-    //     "name": "checkbox1",
-    //     "label": "Enable this"
-    // });
-    // 
-    // checkbox1.addEvent("action", function (value) {
-    //     alert("you toggled me: " + value);
-    // });*/
+    var settings = new FancySettings("My Extension", "icon.png");
+    
+    var checkbox1 = settings.create({
+        "tab": "tabName",
+        "group": "groupName",
+        "type": "checkbox",
+        "name": "checkbox1",
+        "label": "Enable this"
+    });
+    
+    settings.create({
+        "tab": "tabName",
+        "group": "groupName",
+        "type": "button",
+        "label": "Press me softly:",
+        "text": "Right Here"
+    });
+    
+    settings.create({
+        "tab": "Tab 2",
+        "group": "Some Grop",
+        "type": "text",
+        "masked": true,
+        "text": "6 - 12 characters",
+        "label": "Enter password:"
+    });
+    
+    checkbox1.addEvent("action", function (value) {
+        alert("you toggled me: " + value);
+    });
 });
